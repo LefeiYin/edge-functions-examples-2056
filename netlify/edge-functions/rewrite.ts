69d692a04893d247f0d1d26b6d06ad49eac6bc23
LefeiYin <127030449+LefeiYin@users.noreply.github.com>
2023-07-08 01:51:06 +0800

    Update rewrite.ts

--- netlify/edge-functions/rewrite.ts
+++ netlify/edge-functions/rewrite.ts
@@ -1,20 +1,11 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-    //const data = await request.json();
-    let host = 'https://api.openai.com';
-    let newheaders = new Headers(request.headers);
-    newheaders.set('Host', host);
-    newheaders.set('access-control-allow-origin', '*');
-    newheaders.set('access-control-allow-credentials', true);
-    
     let config = {
       headers:request.headers,
       method:'POST',
       body:request.body
     }
-
-    const res = await fetch('https://api.openai.com/v1/chat/completions/',config);
     //return Response.json(res);
-    return res
+    return fetch('https://api.openai.com/v1/chat/completions', config)
 };
