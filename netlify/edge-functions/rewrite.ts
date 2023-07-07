27ea8676dff146d565c089eeea0508d87b099e9d
LefeiYin <127030449+LefeiYin@users.noreply.github.com>
2023-07-07 23:29:10 +0800

    Update rewrite.ts

--- netlify/edge-functions/rewrite.ts
+++ netlify/edge-functions/rewrite.ts
@@ -3,8 +3,6 @@ import type { Context } from "https://edge.netlify.com";
 export default async (request: Request, context: Context) => {
     const body = await request.json();
     //let data = body.data;
-    return new Response(body, {
-        headers: { "content-type": "text/html" },
-    });
+    return context.json(body);
 
 };
