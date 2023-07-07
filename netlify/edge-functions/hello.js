7eae385ad0ae950e97bd9255f9b54c3c74bc0be9
LefeiYin <127030449+LefeiYin@users.noreply.github.com>
2023-07-07 21:10:45 +0800

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,5 @@
 export default async (request) => {
-  return new Response("Hello, World!", {
+  return new Response("Hello, lalallalalala!", {
     headers: { "content-type": "text/html" },
   });
 };
