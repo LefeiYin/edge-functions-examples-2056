1311ecb0d895abc6d03cf512154619a8d8d1b79f
LefeiYin <127030449+LefeiYin@users.noreply.github.com>
2023-07-07 22:02:52 +0800

    Update proxy-requests.ts

--- netlify/edge-functions/proxy-requests.ts
+++ netlify/edge-functions/proxy-requests.ts
@@ -3,7 +3,8 @@ export default async (request: Request, context: Context) => {
   const joke = await fetch("https://chatbot0310.onrender.com/", {
     "headers": {
       "Accept": "application/json"
-    }
-  });
+    },
+    'method': "GET"
+  }).then((res)=>{return res;});
   return new Response(joke);
 };
