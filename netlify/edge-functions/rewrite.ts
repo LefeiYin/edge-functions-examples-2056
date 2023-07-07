839bcbc1c198b42cd836ce612c6e2d6dad5fa9f6
LefeiYin <127030449+LefeiYin@users.noreply.github.com>
2023-07-07 22:40:56 +0800

    Update rewrite.ts

--- netlify/edge-functions/rewrite.ts
+++ netlify/edge-functions/rewrite.ts
@@ -3,6 +3,6 @@ import type { Context } from "https://edge.netlify.com";
 export default async (request: Request, context: Context) => {
     const url = new URL("/", 'https://chatbot0310.onrender.com');
     const res = await fetch(url);
-    const result = {req:JSON.stringify(request.body),res:res}
+    const result = {res:res}
     return result;
 };
