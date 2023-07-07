14679805d47193df0fb673c1615590275419baab
LefeiYin <127030449+LefeiYin@users.noreply.github.com>
2023-07-08 00:06:50 +0800

    Update rewrite.ts

--- netlify/edge-functions/rewrite.ts
+++ netlify/edge-functions/rewrite.ts
@@ -2,22 +2,14 @@ import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
     const data = await request.json();
-    //let data = context.json(body);
-    context.log(data.apikey,data.apikey)
-    let pkg = {
-      'apikey':data.apikey,
-      'data': {
-        'model':'gpt-3.5-turbo',
-        'messages':[{'role':'user','content':data.prompt}]
-      }
-    }
+    context.log(data)
     let config = {
       headers:{
         'Content-Type':'application/json',
         'Authorization': 'Bearer '+ data.apikey,
       },
       method:'POST',
-      body:pkg.data
+      body:data
     }
     const url = new URL("/", 'https://api.openai.com/v1/chat/completions');
     const res = await fetch(url,config);
